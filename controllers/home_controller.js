df6150589978cfa0a2ade889c44f755cb3aeb08b
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2023-05-16 13:15:00 +0530

    created view for home

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,8 @@
 module.exports.home = function(req, res){
-    return res.end('<h1>Express is up for Codeial!</h1>')
+    return res.render('home' ,{
+        title: "Home"
+    })
+    //return res.end('<h1>Express is up for Codeial!</h1>')
 }
 
 //by this we can add more actions syntax
