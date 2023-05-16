307d70879ebfb2ea87292c946255d2afdf724088
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2023-05-16 19:27:04 +0530

    created and altered cookie

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function db() {
   try {
-    await mongoose.connect('mongodb://localhost/codeial_development');
+    await mongoose.connect('mongodb://localhost:27017/codeial_development');
     console.log('Connected to Database :: MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
