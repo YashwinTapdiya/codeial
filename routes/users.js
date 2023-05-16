4bcacef29fb9eddf8c0541c00b8399dd4ca5734d
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2023-05-16 18:26:16 +0530

    Setup user schema and created form for sign-in and sign-up

--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,8 @@ const usersConrtoller = require('../controllers/users_controller');
 
 router.get('/profile', usersConrtoller.profile);
 
+router.get('/sign-up' , usersConrtoller.signUp);
+
+router.get('/sign-in' , usersConrtoller.signIn);
+
 module.exports = router;
