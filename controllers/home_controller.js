0f583c289e5f88162222f7f7f9da411404e65659
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2023-05-16 12:23:45 +0530

    added controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+module.exports.home = function(req, res){
+    return res.end('<h1>Express is up for Codeial!</h1>')
+}
+
+//by this we can add more actions syntax
+// module.exports.actionName = function(req, res){}
