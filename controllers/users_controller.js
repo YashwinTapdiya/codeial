a2c63368f6effd38e3fe1c60fc63f222e5a155ee
Yashwin Tapdiya <yshwintapdiya@gmail.com>
2023-05-19 19:30:00 +0530

    changing branch to main

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -49,7 +49,7 @@ module.exports.profile = async function (req, res) {
 // render the sign up page
 module.exports.signUp = function(req, res){
     return res.render('user_sign_up', {
-        title: "Codeial | Sign Up"
+        title: "InstaBook | Sign Up"
     })
 }
 
@@ -57,7 +57,7 @@ module.exports.signUp = function(req, res){
 // render the sign in page
 module.exports.signIn = function(req, res){
     return res.render('user_sign_in', {
-        title: "Codeial | Sign In"
+        title: "InstaBook | Sign In"
     })
 }
 
