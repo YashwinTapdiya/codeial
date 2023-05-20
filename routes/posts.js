8bef0696319a3e119594c0fc5f2fe7a0a9555355
Yashwin Tapdiya <yshwintapdiya@gmail.com>
2023-05-20 13:31:10 +0530

    check authentication on creating post

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const postsController = require('../controllers/posts_controller');
 
-router.post('/create', postsController.create);
+
+//router.post('/create', postsController.create);
+
+//this will let only sig-in user to post
+router.post('/create', passport.checkAuthentication, postsController.create);
 
 module.exports = router;
