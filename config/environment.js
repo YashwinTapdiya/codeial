1f9b4f02df38b6b7fc4b8c6a47a398218492b654
Yashwin Tapdiya <yashwintapdiya@gmail.com>
2024-02-03 02:58:41 +0530

    setting up production env var

--- config/environment.js
+++ config/environment.js
@@ -21,6 +21,26 @@ const development = {
 
 const production = {
   name: "production",
+  asset_path: process.env.INSTABOOK_ASSET_PATH,
+  session_cookie_key: process.env.INSTABOOK_SESSION_COOKIE_KEY,
+  db: process.env.INSTABOOK_DB,
+  smtp: {
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.INSTABOOK_EMAIL,
+      pass: process.env.INSTABOOK_EMAIL_PASS,
+    },
+  },
+  google_client_id: process.env.INSTABOOK_GOOGLE_CLIENT_ID,
+  google_client_Secret: process.env.INSTABOOK_GOOGLE_SECRET_ID,
+  google_call_back_URL: "http://localhost:8000/users/auth/google/callback",
+  jwt_secret: process.env.INSTABOOK_JWT_SECRET_KEY,
 };
 
-module.exports = development;
+module.exports =
+  eval(process.env.INSTABOOK_ENVIRONMENT) == undefined
+    ? development
+    : eval(process.env.INSTABOOK_ENVIRONMENT);
